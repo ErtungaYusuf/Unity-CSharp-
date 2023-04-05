31a3383ee4d7b39dd0f14755590d76137449f611
Ertunga Yusuf OCAK <115732274+ErtungaYusuf@users.noreply.github.com>
2023-04-05 22:04:04 +0300

    Add files via upload

---
+++ Simple Chess Board Creator Unity C#.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] GameObject Square;
+
+
+    public GameObject[,] squares = new GameObject[9, 9];
+    Color32 blackSquaresColor = new Color32(10, 10, 10, 255);
+    Color32 whiteSquaresColor = new Color32(245, 245, 245, 255);
+
+    bool blackAndWhiteBoardCreationBool = false;
+
+    void CreatBoard()
+    {
+        for (int i = 1; i < 9; i++)
+        {
+            blackAndWhiteBoardCreationBool = !blackAndWhiteBoardCreationBool; // for each row this will change start color
+            for (int j = 1; j < 9; j++)
+            {
+                squares[j, i] = Instantiate(Square, new Vector3(j, i), Quaternion.identity);
+                if (blackAndWhiteBoardCreationBool)
+                {
+                    squares[j, i].GetComponent<SpriteRenderer>().material.color = blackSquaresColor;
+                    blackAndWhiteBoardCreationBool = false;
+                }
+                else
+                {
+                    squares[j, i].GetComponent<SpriteRenderer>().material.color = whiteSquaresColor;
+                    blackAndWhiteBoardCreationBool = true;
+                }
+
+                //  9,1 9,1 9,2 .... 9,9
+                //  8,1
+                //  8,1
+                //   ...
+                //  1,1              
+            }
+        }
+    }
+
+}
